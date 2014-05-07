3e6e0e4f9a218e96c45df2845cfd50fad756ac49
Darron Froese <darron@froese.org>
2014-05-07 14:07:13 -0600

    bump version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       'Darron Froese'
 maintainer_email 'darron@froese.org'
 license          'Apache 2.0'
 description      'Installs/configures gitreceive'
-version          '0.3.0'
+version          '0.4.0'
 recipe           'gitreceive::default', 'Installs/configures gitreceive'
 
 depends 'apt'
